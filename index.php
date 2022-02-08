7e7f7de2fa29d12a2e387182c08af94ebc5b8cd5
Fahdroussafi <cmpunk589@outlook.com>
2022-02-08 10:02:22 +0100

    update

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <?php
 include 'includes/class-autoload.inc.php';
-echo '<link rel="stylesheet" type="text/css" href="style.css">';
 
-// $cow  = new Cow('Herbivore', 'Grass');
-// $lion = new Lion('Canirval', 'Meat');
-// echo '<div>';
-// echo '<b>Cow Object</b> <br>';
-// echo '<p>The Cow belongs to the ' . $cow->get_family() . ' family and eats ' . $cow->get_food() . '<br><br>';
-// echo '<b>Lion Object</b> <br>';
-// echo '<p>The Lion belongs to the ' . $lion->get_family() . ' family and eats ' . $lion->get_food();
-// echo '</div>';
 ?>
 <!DOCTYPE html>
 <html lang="en">
